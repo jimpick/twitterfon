4ae9e068a117d449c56bb64249bfa047466ed330
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-08-27 02:43:29 +0000

    * Re-write cache cleanup routine
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1001 542e9493-1a22-0410-9183-c10453d2b9ee

--- Classes/DB/DBConnection.m
+++ Classes/DB/DBConnection.m
@@ -1,4 +1,5 @@
 #import "DBConnection.h"
+#import "TimeUtils.h"
 
 static sqlite3*             theDatabase = nil;
 
@@ -7,28 +8,33 @@ static sqlite3*             theDatabase = nil;
 + (sqlite3*)getSharedDatabase
 {
     if (theDatabase == nil) {
+        Stopwatch *s = [Stopwatch stopwatch];
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *documentsDirectory = [paths objectAtIndex:0];
         NSString *path = [documentsDirectory stringByAppendingPathComponent:@"db1.1.sql"];
         // Open the database. The database was prepared outside the application.
+        [s lap:@"Open DB..."];
         if (sqlite3_open([path UTF8String], &theDatabase) != SQLITE_OK) {
             // Even though the open failed, call close to properly clean up resources.
             sqlite3_close(theDatabase);
             NSAssert1(0, @"Failed to open database with message '%s'.", sqlite3_errmsg(theDatabase));
             // Additional error handling, as appropriate...
         }
-
+        [s lap:@"Init DB"];
         [DBConnection deleteOldCache:theDatabase];
+        [s lap:@"clear cache"];
     }
     return theDatabase;
 }
 
-const char * sqls[4] = {
+#define NUM_QUERIES 4
+
+const char * sqls[NUM_QUERIES] = {
     "DELETE FROM images WHERE updated_at <= (SELECT updated_at FROM images order by updated_at LIMIT 1 OFFSET 1000)",
-#if 1
+#if 1   
     "DELETE FROM messages WHERE type = 0 and id <= (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 200)",
     "DELETE FROM messages WHERE type = 1 and id <= (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 200)",
-    "DELETE FROM messages WHERE type = 2 and id <= (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 200)"
+    "DELETE FROM messages WHERE type = 2 and id <= (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 200)",
 #else
     "DELETE FROM messages WHERE type = 0 and id > (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 20)",
     "DELETE FROM messages WHERE type = 1 and id >= (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 5)",
@@ -38,19 +44,14 @@ const char * sqls[4] = {
 
 + (void)deleteOldCache:(sqlite3*)db
 {
-    sqlite3_stmt* statement;
     int i;
     int success;
+    char *errmsg;
     
-    for (i = 0; i < 4; ++i) {
-        if (sqlite3_prepare_v2(db, sqls[i], -1, &statement, NULL) != SQLITE_OK) {
-            NSLog(@"%s", sqlite3_errmsg(db));
-            NSAssert1(0, @"Error: failed to prepare statement with message '%s'.", sqlite3_errmsg(db));
-        }
-        success = sqlite3_step(statement);
-        sqlite3_finalize(statement);
-        if (success != SQLITE_DONE) {
-            NSAssert1(0, @"Error: failed to delete from database with message '%s'.", sqlite3_errmsg(db));
+    for (i = 0; i < NUM_QUERIES; ++i) {
+        success = sqlite3_exec(db, sqls[i], NULL, NULL, &errmsg);
+        if (success != SQLITE_OK) {
+            NSAssert1(0, @"Error: failed to cleanup chache (%s)", errmsg);
         }
     }
 }
