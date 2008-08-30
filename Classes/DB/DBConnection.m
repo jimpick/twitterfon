857bcefb443ead3ff11c6d2c7698c1daf476c86e
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-08-30 01:35:43 +0000

    * Added test code which delete local caches
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1040 542e9493-1a22-0410-9183-c10453d2b9ee

--- Classes/DB/DBConnection.m
+++ Classes/DB/DBConnection.m
@@ -2,6 +2,17 @@
 
 static sqlite3*             theDatabase = nil;
 
+//#define TEST_DELETE_TWEET
+
+#ifdef TEST_DELETE_TWEET
+const char *delete_tweets = 
+"BEGIN;"
+"DELETE FROM messages WHERE type = 0 and id > (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 80);"
+"DELETE FROM messages WHERE type = 1 and id > (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 100);"
+"DELETE FROM messages WHERE type = 2 and id > (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 35);"
+"COMMIT";
+#endif
+
 @implementation DBConnection
 
 + (sqlite3*)getSharedDatabase
@@ -17,21 +28,25 @@ static sqlite3*             theDatabase = nil;
             NSAssert1(0, @"Failed to open database with message '%s'.", sqlite3_errmsg(theDatabase));
             // Additional error handling, as appropriate...
         }
+#ifdef TEST_DELETE_TWEET
+        char *errmsg;
+        
+        if (sqlite3_exec(theDatabase, delete_tweets, NULL, NULL, &errmsg) != SQLITE_OK) {
+            NSAssert1(0, @"Error: failed to cleanup chache (%s)", errmsg);
+        }
+#endif        
+        
     }
     return theDatabase;
 }
 
 const char *cleanup_sql =
-    "BEGIN;"
-    "DELETE FROM images WHERE updated_at <= (SELECT updated_at FROM images order by updated_at LIMIT 1 OFFSET 1000);"
-    "DELETE FROM messages WHERE type = 0 and id <= (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 200);"
-    "DELETE FROM messages WHERE type = 1 and id <= (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 200);"
-    "DELETE FROM messages WHERE type = 2 and id <= (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 200);"
-//    "DELETE FROM messages WHERE type = 0 and id > (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 67);"
-//    "DELETE FROM messages WHERE type = 1 and id > (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 5);"
-//    "DELETE FROM messages WHERE type = 2 and id > (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 5);"
-    "COMMIT"
-;
+"BEGIN;"
+"DELETE FROM images WHERE updated_at <= (SELECT updated_at FROM images order by updated_at LIMIT 1 OFFSET 1000);"
+"DELETE FROM messages WHERE type = 0 and id <= (SELECT id FROM messages WHERE type = 0 ORDER BY id DESC LIMIT 1 OFFSET 200);"
+"DELETE FROM messages WHERE type = 1 and id <= (SELECT id FROM messages WHERE type = 1 ORDER BY id DESC LIMIT 1 OFFSET 200);"
+"DELETE FROM messages WHERE type = 2 and id <= (SELECT id FROM messages WHERE type = 2 ORDER BY id DESC LIMIT 1 OFFSET 200);"
+"COMMIT";
 
 + (void)deleteOldCache
 {
