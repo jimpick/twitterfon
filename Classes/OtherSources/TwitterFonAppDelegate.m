bf1af57a4b022ed165e069fd527aed8599bf080d
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-08-30 08:45:20 +0000

    * Remove obsolete test code  * Using macro for database name
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1042 542e9493-1a22-0410-9183-c10453d2b9ee

--- Classes/OtherSources/TwitterFonAppDelegate.m
+++ Classes/OtherSources/TwitterFonAppDelegate.m
@@ -32,10 +32,6 @@
     // so we need to create a copy of it in the application's Documents directory.     
     [self createEditableCopyOfDatabaseIfNeeded];
 
-#if 0 // for debugging
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"username"];
-    [[NSUserDefaults standardUserDefaults] removeObjectForKey:@"password"];
-#endif
 	NSString *username = [[NSUserDefaults standardUserDefaults] stringForKey:@"username"];
 	NSString *password = [[NSUserDefaults standardUserDefaults] stringForKey:@"password"];
 
@@ -138,11 +134,11 @@
     NSError *error;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *writableDBPath = [documentsDirectory stringByAppendingPathComponent:@"db1.1.sql"];
+    NSString *writableDBPath = [documentsDirectory stringByAppendingPathComponent:MAIN_DATABASE_NAME];
     success = [fileManager fileExistsAtPath:writableDBPath];
     if (success) return;
     // The writable database does not exist, so copy the default to the appropriate location.
-    NSString *defaultDBPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"db1.1.sql"];
+    NSString *defaultDBPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:MAIN_DATABASE_NAME];
     success = [fileManager copyItemAtPath:defaultDBPath toPath:writableDBPath error:&error];
     if (!success) {
         NSAssert1(0, @"Failed to create writable database file with message '%@'.", [error localizedDescription]);
