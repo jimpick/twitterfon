f2e7200d15fdc768309ebd57ba3cee78faad9830
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-10-25 07:48:30 +0000

    * Re-design post view background color
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1081 542e9493-1a22-0410-9183-c10453d2b9ee

---
+++ Classes/Controllers/PostView.h
@@ -0,0 +1,16 @@
+//
+//  PostView.h
+//  TwitterFon
+//
+//  Created by kaz on 10/25/08.
+//  Copyright 2008 naan studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PostView : UIView {
+    CGImageRef              background;
+}
+
+@end
---
+++ Classes/Controllers/PostView.m
@@ -0,0 +1,41 @@
+//
+//  PostView.m
+//  TwitterFon
+//
+//  Created by kaz on 10/25/08.
+//  Copyright 2008 naan studio. All rights reserved.
+//
+
+#import "PostView.h"
+
+
+@implementation PostView
+
+
+- (id)initWithFrame:(CGRect)frame {
+    if (self = [super initWithFrame:frame]) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    UIImage *img = [UIImage imageNamed:@"usercell_background.png"];
+    background = CGImageRetain(img.CGImage);
+}
+
+
+- (void)drawRect:(CGRect)rect {
+    CGContextRef context = UIGraphicsGetCurrentContext();
+  	CGContextDrawImage(context, rect, background);
+}
+
+
+- (void)dealloc {
+    CGImageRelease(background);
+    [super dealloc];
+}
+
+
+@end
