b9bce11de94be1f71ba85a0fbf9e0763190d08ad
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-11-17 06:41:21 +0000

    * Save description to local DB  * unescape user location and description
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1250 542e9493-1a22-0410-9183-c10453d2b9ee

--- Classes/Twitter/User.m
+++ Classes/Twitter/User.m
@@ -1,5 +1,6 @@
 #import "User.h"
 #import "DBConnection.h"
+#import "StringUtil.h"
 
 @implementation User
 
@@ -22,18 +23,20 @@
     name            = [[dic objectForKey:@"name"] retain];
 	screenName      = [[dic objectForKey:@"screen_name"] retain];
 	location        = [[dic objectForKey:@"location"] retain];
-//	description     = [[dic objectForKey:@"description"] retain];
+	description     = [[dic objectForKey:@"description"] retain];
 	url             = [[dic objectForKey:@"url"] retain];
     followersCount  = [[dic objectForKey:@"followers_count"] longValue];
     profileImageUrl = [[dic objectForKey:@"profile_image_url"] retain];
     protected       = [[dic objectForKey:@"protected"] boolValue];
-    description     = @"";
 
     if ((id)name == [NSNull null]) name = @"";
     if ((id)screenName == [NSNull null]) screenName = @"";
     if ((id)location == [NSNull null]) location = @"";
-//    if ((id)description == [NSNull null]) description = @"";
+    if ((id)description == [NSNull null]) description = @"";
     if ((id)url == [NSNull null]) url = @"";
+    
+    self.location    = [location unescapeHTML];
+    self.description = [description unescapeHTML];
 	
 	return self;
 }
