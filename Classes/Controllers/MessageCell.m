7fb5d8b84d3c54a3e3c09e73e0a73083b2af2442
kaz <kaz@542e9493-1a22-0410-9183-c10453d2b9ee>
2008-11-22 07:20:59 +0000

    * Adjusted profile image icon vertical position
    git-svn-id: http://naan.net/svn/trunk/TwitterFon@1304 542e9493-1a22-0410-9183-c10453d2b9ee

--- Classes/Controllers/MessageCell.m
+++ Classes/Controllers/MessageCell.m
@@ -124,7 +124,7 @@ static NSString *nameRegexp = @"(@[0-9a-zA-Z_]+)";
         profileImage.frame = CGRectMake(2, 0, 38, message.cellHeight);
     }
     else {
-        profileImage.frame = CGRectMake(IMAGE_PADDING, 0, IMAGE_WIDTH, message.cellHeight);
+        profileImage.frame = CGRectMake(IMAGE_PADDING, (message.cellHeight - 48 - 1)/2, IMAGE_WIDTH, 48);
     }
 }
 
@@ -134,17 +134,9 @@ static NSString *nameRegexp = @"(@[0-9a-zA-Z_]+)";
     [super setEditing:editing animated:animated];
     
     if (type == MSG_TYPE_USER && animated && (editing || inEditing)) {
-#if 0
-        CATransition *animation = [CATransition animation];
-        [animation setType:kCATransitionFade];
-        [animation setDuration:0.3];
-        [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
-        [profileImage.layer addAnimation:animation forKey:@"favoriteButton"];
-#endif
-        profileImage.hidden = editing;
-        
-        [UIView beginAnimations:nil context:nil];
 
+        profileImage.hidden = editing;
+        [UIView beginAnimations:nil context:nil];
         int x = (editing) ? 10 : 42;
         cellView.frame = CGRectMake(x, cellView.frame.origin.y, cellView.frame.size.width, cellView.frame.size.height);
         [UIView commitAnimations];
